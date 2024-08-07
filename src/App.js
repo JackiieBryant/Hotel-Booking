4faf2ad044bb639ed71037d908c9e976c4023475
Kijanaa <irene.yanapouth@gmail.com>
2024-08-08 01:02:54 +0200

    j'ai creer le reducer, et terminer le premiére partie du front

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from "./booking/Home";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import Navbar from './components/Navbar';
 
 
 
 function App () {
 return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route element={<Home />} path='/' />
         <Route element={<Login />} path='/login' />
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { act } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// 1. Import from react-redux and redux
+import { createStore} from 'redux';
+import { Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from './reducers';
+
+// 2. Create user reducer function
+
+
+
+// 3. Combine multiple reducers
+
+
+// 4. Create redux store
+const store = createStore(rootReducer, composeWithDevTools());
+// 5. Provice redux store to entire app
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+       <App />
+    </Provider>
   </React.StrictMode>
 );
 
