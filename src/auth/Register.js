43bfb335be0a8f43e806cd03137399ede2077e0a
Kijanaa <irene.yanapouth@gmail.com>
2024-08-13 23:34:24 +0200

    form

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,5 +1,35 @@
+import {useState} from "react"
+
 const Register = () => {
-    return <div className="container-fluid h1 p-5 text-center" >Register Page</div>
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = () => {
+        alert('send user info to backend')
+    };
+
+    const RegisterForm = () => <form onSubmit={handleSubmit}>Show the form</form>
+    
+        return(
+        <>
+        <div className="container-fluid h1 bg-secondary p-5 text-center" >
+            <h1>Register</h1>
+        </div> 
+
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    {RegisterForm()}
+                </div>
+
+            </div>
+        </div>
+        </>
+        );
+
+
+    
 }
 
-export default Register
+export default Register;
